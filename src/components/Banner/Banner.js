2dd919ec326896e293b371f2c7dae7dd04190897
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-05-11 19:46:04 -0300

    v5

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -4,19 +4,20 @@ export default function Banner () {
     return (
         <DivBanner>
             <div>
-                <img class="img-banner"src="https://cdn.autopapo.com.br/box/uploads/2019/05/02170928/vw-up-2020.jpg" alt="vw take up"></img>
+                <img src="https://cdn.autopapo.com.br/box/uploads/2019/05/02170928/vw-up-2020.jpg" alt="vw take up"></img>
             </div>
         </DivBanner>
     )
 }
 
 const DivBanner = styled.div`
-   width: 920px;
-   height: 420px;
+
+   width: 720px;
+   height: 360px;
+   align-items: center;
    margin: 0 auto;
-   .img {
-    width: 920px;
-   height: 420px;
-   margin: 0 auto;
-   }
+   justify-content: center;
+
+
+
 `
