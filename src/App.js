fbbc305ccd4b231ebcce4e4350a48efefd41e132
Vinicios Falqueiro Reis <vinicios.falqueiro@gmail.com>
2021-05-11 19:40:19 -0300

    v4

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 
 //import MyButton from './components/button'
 import Header from './components/header/Header'
+import Banner from './components/Banner/Banner'
 
 function App() {
   return (
     <>
       <Header/>
+      <Banner/>
     </>
   )
 }
